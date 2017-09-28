384ab98191aaed207cf88695549aca334bd8b289
Elliot Maude <maude.elliot@gmail.com>
2017-09-27 22:01:17 -0400

    Assignement 2 update

--- assignment2a.js
+++ assignment2a.js
@@ -1,7 +1,27 @@
+/*******************************************************************************
 
-/*****************************
- * Step 1
- *****************************/
+ *                     WEB222 – Assignment 2
+
+ * I declare that this assignment is my own work in accordance with Seneca
+
+ * Academic Policy.  No part of this assignment has been copied manually or
+
+ * electronically from any other source (including web sites) or distributed to
+
+ * other students.
+
+ *
+
+ * Name: Elliot Maude Student ID: 032830127 Date: 25/09/17
+
+ *
+
+ ******************************************************************************/
+
+
+ /*****************************
+  * Step 1
+  *****************************/
 
 var e1, e2, e3, e4, e5, e6, e7, str;
 
@@ -11,45 +31,55 @@ var e1, e2, e3, e4, e5, e6, e7, str;
 
 function capFirstLetter(str)
 {
-	if(typeof(str) === "string")
-		{
-			var first = str.substr(0, 1);
-			var rest = str.substring(1, str.length);
-			first = first.toUpperCase();
-			rest = rest.toLowerCase();
-			
-			return first + rest;
-		}
+  var first = str.substr(0,1);
+  var str = str.substr(1, str.length);
+  first = first.toUpperCase();
+  str = str.toLowerCase();
+  return first + str;
 }
 
-e1 = prompt("Enter First Name", "elliot");
+e1 = prompt("Enter you name: ", "elLiot");
 e1 = capFirstLetter(e1);
+//console.log(e1);
 
 /*****************************
  * Step 3
  *****************************/
 
-var getAge = function(birthYear)
+var getAge = function(year)
 {
-	var today = new Date(); 
-	return today.getFullYear() - birthYear;
-};
+  var current = (new Date()).getFullYear();
+  return current - parseInt(year);
+}
 
-e2 = prompt("Enter Your Birth Year", "1994");
-
+e2 = prompt("Enter your birth year: ", "1994");
 e2 = getAge(e2);
+//console.log(e2);
 
 /*****************************
  * Step 4
  *****************************/
 
-e3 = prompt("Enter College Name");
+e3 = prompt("Enter your college name", "seneca college");
 
 e3 = e3.split(" ");
+capFirstLetter(e3[0]);
+capFirstLetter(e3[1]);
 
-for(var i in e3){
-	e3.push(capFirstLetter(i));
-}
+e3 = e3[0] + e3[1];
 
-console.log(e3);
+/*****************************
+ * Step 5
+ *****************************/
 
+e4 = prompt("Enter your 5 favorite sports (lower case, Comma seperated)", "hockey,football,basketball,tennis,golf");
+
+var patt = /football/;
+e4 = e4.replace(patt, "soccer");
+
+e4 = e4.split(',');
+
+e5 = prompt("Enter another favorite sport", "formula 1");
+e4.push(e5);
+
+//console.log(e4);
